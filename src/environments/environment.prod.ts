f8a2ebab3ac067dceeefb107b140f77ad3291f5e
Danilo Silva <danilo.silva@msn.com>
2025-06-02 19:13:40 -0300

    fix: Adiciona arquivos environment necessarios para build - Remove environment.ts do gitignore - Arquivos necessarios para CI/CD

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBIKRdNjENZGD6K7D2UvOQUR6-llveqxzs',
+    authDomain: 'vai-na-lista-f9551.firebaseapp.com',
+    projectId: 'vai-na-lista-f9551',
+    storageBucket: 'vai-na-lista-f9551.firebasestorage.app',
+    messagingSenderId: '299674883410',
+    appId: '1:299674883410:web:3a65734bee3666d4ab568b',
+  },
+};
